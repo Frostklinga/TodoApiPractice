ee38a8dcf5dac62ba6b90dd18bdc406e267b3dc5
Daniel Frostklinga <daniel@frostklinga.se>
2023-02-23 14:54:08 +0100

    Added a console application to consume todo items.

---
+++ DataAccess/FileStorage.cs
@@ -0,0 +1,50 @@
+﻿using Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DataAccess
+{
+    public class FileStorage : IDataAccess
+    {
+        public readonly string filePath = @".\";
+        public readonly string storageFileName = "datastore.json";
+        public FileStorage()
+        {
+            
+        }
+        public void Add(TodoModel todo)
+        {
+            var todoAsJson = JsonConvert.SerializeObject(todo, Formatting.Indented);
+            File.WriteAllText(filePath, todoAsJson);
+        }
+
+        public void Delete(TodoModel todo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoModel GetAllTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TodoModel todo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TodoConsoleConsumer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+
+namespace TodoConsoleConsumer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+        }
+    }
+}
